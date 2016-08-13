eb8971f875a746629f80bbf07de777c695789691
Wenson Tsai <wensontsai@gmail.com>
2016-08-13 16:23:37 -0400

    adding guardian to mix

--- web/models/user.ex
+++ web/models/user.ex
@@ -29,5 +29,15 @@ defmodule PhoenixTrello.User do
     |> validate_length(:password, min: 5)
     |> validate_confirmation(:password, message: "Password does not match")
     |> unique_constraint(:email, message: "Email already taken")
+    |> generate_encrypted_password
   end
+
+  defp generate_encrypted_password(current_changeset) do
+      case current_changeset do
+        %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+          put_change(current_changeset, :encrypted_password, Comeonin.Bcrypt.hashpwsalt(password))
+        _ ->
+          current_changeset
+      end
+    end
 end
