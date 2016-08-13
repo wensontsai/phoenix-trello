57afa7eb69fe31fec30ff258c8f7058ec4d766eb
Wenson Tsai <wensontsai@gmail.com>
2016-08-13 16:04:25 -0400

    added comeonin

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule PhoenixTrello.Mixfile do
   def application do
     [mod: {PhoenixTrello, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :comeonin]]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,13 +30,16 @@ defmodule PhoenixTrello.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.1.4"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_ecto, "~> 2.0"},
-     {:phoenix_html, "~> 2.4"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.9"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:phoenix, "~> 1.1.4"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_ecto, "~> 2.0"},
+      {:phoenix_html, "~> 2.4"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.9"},
+      {:cowboy, "~> 1.0"},
+      {:comeonin, "~> 2.0"}
+   ]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
