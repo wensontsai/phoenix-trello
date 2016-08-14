3721995369d4164530644fc9ed7219a3b0bc04d4
Wenson Tsai <wensontsai@gmail.com>
2016-08-14 15:28:51 -0400

    added seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,17 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+
+alias PhoenixTrello.{Repo, User}
+
+[
+  %{
+    first_name: "John",
+    last_name: "Bloke",
+    email: "john@phoenix-trello.com",
+    password: "12345678"
+  },
+]
+|> Enum.map(&User.changeset(%User{}, &1))
+|> Enum.each(&Repo.insert!(&1))
